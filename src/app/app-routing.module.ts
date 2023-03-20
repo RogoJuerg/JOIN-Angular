f8814325c96e4c1ecf40ed735b41391223d87a7e
RogoJuerg <juergen.rogozin@gmail.com>
2023-03-20 20:34:22 +0100

    created components, added side-menu classes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { BoardComponent } from './board/board.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'summary', component: SummaryComponent},
+  {path: 'board', component: BoardComponent},
+  {path: 'add-task', component: AddTaskComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'legal-notice', component: LegalNoticeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { SummaryComponent } from './summary/summary.component';
+import { BoardComponent } from './board/board.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SideMenuComponent,
+    SummaryComponent,
+    BoardComponent,
+    AddTaskComponent,
+    ContactsComponent,
+    LegalNoticeComponent
   ],
   imports: [
     BrowserModule,
